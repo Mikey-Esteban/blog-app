9488e9683b7a998a373846c1e31870070926240f
Mikey Esteban <michaelp.esteban@gmail.com>
2021-01-22 13:01:07 -0500

    editing Post title, body values if too short

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true
 
-  before_create :slugify, :check_image_url
+  before_create :slugify, :check_image_url, :check_title, :check_body
 
   def slugify
     self.slug = title.parameterize
@@ -17,7 +17,23 @@ class Post < ApplicationRecord
     end
   end
 
-  def total_comments
-    comments.length
+  def check_title
+    if title.length < 3
+      title = Faker::Hipster.sentence(word_count: 3, supplemental: true, random_words_to_add: 4)
+      self.title = title
+    end
   end
+
+  def check_body
+    if body.length < 3
+      body = ''
+      rand(3...10).times do
+        paragraph = Faker::Hipster.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 6)
+        paragraph += "\n"
+        body += paragraph
+      end
+      self.body = body
+    end
+  end
+
 end
