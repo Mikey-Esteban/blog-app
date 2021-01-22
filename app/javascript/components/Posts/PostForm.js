3d99f2511b08574035283161bd05a8a6d724abfd
Mikey Esteban <michaelp.esteban@gmail.com>
2021-01-22 18:33:02 -0500

    added form helpers to PostForm

--- app/javascript/components/Posts/PostForm.js
+++ app/javascript/components/Posts/PostForm.js
@@ -34,9 +34,10 @@ const Field = styled.div`
     border: 1px solid #efefef;
     border-radius: 4px;
     height: 30px;
-    line-height: 30px;
     padding-left: 10px;
     font-size: 14px;
+    font-family: 'Montserrat', sans-serif;
+    line-height: 30px;
   }
 
   textarea {
@@ -61,6 +62,12 @@ const Field = styled.div`
     color: #fff;
   }
 `
+const Helper = styled.span`
+
+  color: rgb(255, 255, 255);
+  font-size: 14px;
+  transition: all ease-in-out 200ms;
+`
 
 const PostForm = () => {
 
@@ -83,6 +90,13 @@ const PostForm = () => {
       .catch( resp => console.log(resp) )
   }
 
+  const handleMouseOverOrOut = e => {
+    const label = e.target.previousSibling
+    const helper = label.lastChild
+    let color = helper.style.color
+    color === '' || color === 'rgb(255, 255, 255)' ? helper.style.color = 'rgb(224, 122, 95)' : helper.style.color = 'rgb(255, 255, 255)'
+  }
+
   if (redirect) {
   return <Redirect to='/'/>;
   }
@@ -97,18 +111,24 @@ const PostForm = () => {
       <Wrapper>
         <form onSubmit={handleSubmit} >
           <Field>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" onChange={handleChange} value={post.title || ''} placeholder='Attention grabbing title!'/>
+            <label htmlFor="title">Title <Helper>If less than 3 characters, title will be generated</Helper></label>
+            <input type="text" name="title"
+              onMouseOver={handleMouseOverOrOut} onMouseOut={handleMouseOverOrOut}
+              onChange={handleChange} value={post.title || ''} placeholder='Attention grabbing title!'/>
           </Field>
 
           <Field>
-            <label htmlFor="image-url">Image</label>
-            <input type="text" name="image_url" onChange={handleChange} value={post.image_url || ''} placeholder='Have an image? Link us!'/>
+            <label htmlFor="image-url">Image <Helper>If left blank, image url will be generated from unsplash.com</Helper></label>
+            <input type="text" name="image_url"
+              onMouseOver={handleMouseOverOrOut} onMouseOut={handleMouseOverOrOut}
+              onChange={handleChange} value={post.image_url || ''} placeholder='Have an image? Link us!'/>
           </Field>
 
           <Field>
-            <label htmlFor="body">Body</label>
-            <textarea name="body" onChange={handleChange} value={post.body || ''} placeholder='Share your knowledge and expertise'/>
+            <label htmlFor="body">Body <Helper>If less than 3 characters, body will be generated</Helper></label>
+            <textarea name="body"
+              onMouseOver={handleMouseOverOrOut} onMouseOut={handleMouseOverOrOut}
+              onChange={handleChange} value={post.body || ''} placeholder='Share your knowledge and expertise'/>
           </Field>
 
           <Field>
